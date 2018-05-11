05d53e84ff3603a998018e97a7efd9779039c734
Trent <trent.stenoien@gmail.com>
2018-05-11 05:17:37 -0500

    Basic loans views and routing

--- routes/loans.js
+++ routes/loans.js
@@ -3,7 +3,27 @@ var loansRouter = express.Router();
 
 /* GET home page. */
 loansRouter.get('/', function(req, res, next) {
-  res.render('home', { title: 'loans' });
+  res.redirect('/loans/all');
+});
+
+loansRouter.get('/all', function(req, res, next) {
+  var view = require.resolve('../views/loans/all.pug');
+  res.render(view, { title: 'All Loans' });
+});
+
+loansRouter.get('/overdue', function(req, res, next) {
+  var view = require.resolve('../views/loans/overdue.pug');
+  res.render(view, { title: 'Overdue Loans' });
+});
+
+loansRouter.get('/checked_out', function(req, res, next) {
+  var view = require.resolve('../views/loans/checked.pug');
+  res.render(view, { title: 'Loaned Books' });
+});
+
+loansRouter.get('/new', function(req, res, next) {
+  var view = require.resolve('../views/loans/new.pug');
+  res.render(view, { title: 'New Loan' });
 });
 
 module.exports = loansRouter;
