7bf1f31377f047629ac8da4eb31d5a13fe1bd926
Trent <trent.stenoien@gmail.com>
2018-05-11 12:39:36 -0500

    Error handling and routing fixes

--- app.js
+++ app.js
@@ -39,9 +39,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  console.log(res.locals);
   res.render('error', {
-    title: `Error: ${err.status}`,
-    message: res.locals.message
+    title: err.message,
+    status: res.statusCode,
+    message: err.toString()
   });
 });
 
