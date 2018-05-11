8d73af5b8ea86ec2c3e559b8b0591f8291d344c5
Trent <trent.stenoien@gmail.com>
2018-05-11 11:48:19 -0500

    Public folder for scripts and styles

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('./stylesheets'));
+app.use(express.static('./public'));
 
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
