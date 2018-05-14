80239df5dc480a32255625f0fbd8f822fded9656
Trent <trent.stenoien@gmail.com>
2018-05-14 18:38:42 -0500

    I missed a couple of files

--- routes/patrons.js
+++ routes/patrons.js
@@ -1,27 +1,55 @@
-var express = require('express');
-var patronsRouter = express.Router();
+const express = require('express');
+const patronsRouter = express.Router();
+const Patron = require('../models').Patron;
 
 /* REDIRECT to all patrons. */
-patronsRouter.get('/', function(req, res, next) {
+patronsRouter.get('/', (req, res, next) => {
   res.redirect('/patrons/all');
 });
 
 /* GET all patrons. */
-patronsRouter.get('/all', function(req, res, next) {
-  var view = require.resolve('../views/patrons/all.pug');
-  res.render(view, { title: 'All Patrons' });
+patronsRouter.get('/all', (req, res, next) => {
+  Patron.findAll().then(patrons => {
+    res.render('patrons/patron-list', {
+      title: 'All Patrons',
+      patrons
+    });
+  });
 });
 
 /* GET form to create a new patron. */
-patronsRouter.get('/new', function(req, res, next) {
-  var view = require.resolve('../views/patrons/details.pug');
-  res.render(view, { title: 'New Patron' });
+patronsRouter.get('/details/new', (req, res, next) => {
+  res.render('patrons/patron-details', {
+    title: 'New Patron',
+    patron: {}
+  });
+});
+
+/* POST form to create a new patron. */
+patronsRouter.post('/details/new', (req, res, next) => {
+  Patron.create(req.body)
+    .then(patron => {
+      res.redirect('/patrons/details/' + patron.id);
+  });
 });
 
 /* GET patron details. */
-patronsRouter.get('/details', function(req, res, next) {
-  var view = require.resolve('../views/patrons/loans.pug');
-  res.render(view, { title: 'Patron Details' });
+patronsRouter.get('/details/:id', (req, res, next) => {
+  Patron.findById(req.params.id)
+    .then(patron => {
+      res.render('patrons/patron-loans', {
+        title: patron.dataValues.title,
+        patron: patron.dataValues,
+        loans: []
+      });
+    });
+});
+
+/* POST new patron details to update its DB row. */
+patronsRouter.post('/details/:id', (req, res, next) => {
+  Patron.findById(req.params.id)
+    .then(patron => patron.update(req.body))
+    .then(patron => res.redirect('/patrons/details/' + patron.id));
 });
 
 module.exports = patronsRouter;
